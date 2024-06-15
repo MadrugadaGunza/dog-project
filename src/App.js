e806aaf22801799caa454f979809e80fe9d24afe
Madrugada de Carvalho Calunga Gunza <madrugadaoca@gmail.com>
2024-06-15 16:43:21 +0100

    feat: limpando o projecto

--- src/App.js
+++ src/App.js
@@ -2,21 +2,14 @@
 import './App.css';
 // dependencies
 import React from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import { BrowserRouter, Route } from 'react-router-dom';
-import { Routes } from 'react-router-dom';
-import Home from './pages/Home';
+// components
+
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+    <div>
+      app
+    </div>
   );
 }
 
